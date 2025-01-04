308c88844fab1325f770e03793160189bbdd6776
SanabelMaali <sanabelmaali6@gmail.com>
2025-01-05 00:48:47 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    
+    fontFamily:{
+      Inter:['Inter','sans-serif']
+    },
+    extend: {
+      screens:{
+        '2md':'960px'
+      },
+      colors:{
+        primary:'#7E3AF2',
+        secondaryText:'#6B7280',
+        mainText:'#111928'
+      },
+    },
+  },
+  plugins: [],
+}
+
